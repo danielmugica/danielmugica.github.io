b34c572cc78e360f9895c5100970fde70db21ecb
danielmugica <55281173+danielmugica@users.noreply.github.com>
2020-01-22 07:59:48 -0500

    Update myScripts.js

--- test/myScripts.js
+++ test/myScripts.js
@@ -4,12 +4,3 @@ for (i = 2; i < 257; i=i*2) {
   text += "The number is " + i + "<br>";
 }
 document.getElementById("demo").innerHTML = text;
-<br>
-  function myLoop() {
-var end = 
- document.getElementById("endLoop")
-for ( var i = 0; i <= 10; i++)
-  {
-    document.write('<p>' + i + '</p>')
-  }
-}
